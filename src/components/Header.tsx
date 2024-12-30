8a4824f49b940d2a60df5064d81fd5d5f9d735d8
mchmddiazz <pomcat.14@gmail.com>
2024-12-31 00:23:46 +0700

    add link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,18 +6,26 @@ const Header: React.FC = () => {
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
-        
+        <Link href="/">
+          <a>MyWebsite</a>
+        </Link>
       </div>
       <nav className={styles.nav}>
         <ul>
           <li>
-            
+            <Link href="/">
+              <a>Home</a>
+            </Link>
           </li>
           <li>
-            
+            <Link href="/about">
+              <a>About</a>
+            </Link>
           </li>
           <li>
-            
+            <Link href="/contact">
+              <a>Contact</a>
+            </Link>
           </li>
         </ul>
       </nav>
